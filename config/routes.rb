3804fb82c4fdedc6e39fd67b0a022188e266dfc1
Roman Turner <rn.turner@live.com>
2021-04-28 13:50:17 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :students, only: [:index, :show] 
+get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
+
+
+#* singuluar resource examples: 
+# Passing a string to 'to:' will expect a controller#action format. 
+# Passing a symbol to the to: option should be replaced with "action: "
+# When using a string witout the # the to: 
